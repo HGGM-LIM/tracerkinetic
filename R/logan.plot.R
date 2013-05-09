f6df7e50c0772e8bc1d01ccb2998ace2b7e68268
José María Mateos <chema@rinzewind.org>
2013-05-09 13:03:38 +0200

    Recoded Logan plot to use the previously commited helper functions

--- R/logan.plot.R
+++ R/logan.plot.R
@@ -25,67 +25,20 @@
 logan.plot <- function(input.function, tissue, time.start, time.end, 
                        plot = TRUE, ...) {
     
-    tmid <- (time.start + time.end) / 2    
-    input.function <- interpolate.tac(input.function, time.start, time.end)$y      
-    tissue <- interpolate.tac(tissue, time.start, time.end)$y        
-    input.function <- input.function[tmid + 1]
-    tissue <- tissue[tmid + 1]                     
-                                         
-    frame.length <- time.end - time.start
-    term1 <- cumsum(tissue * frame.length) / tissue 
-    term2 <- cumsum(input.function * frame.length) / tissue             
-    #term1 <- cumsum(tissue) / tissue 
-    #term2 <- cumsum(input.function) / tissue             
-    #term1 <- term1[tmid + 1]
-    #term2 <- term2[tmid + 1]
-    #l <- length(term1)    
-    l <- length(term1)    
-            
-    # Find optimal time.regression point
-    error <- rep(NA, l)
-    time.regression <- l - 1
+    # Compute data on both axis
+    dt <- time.end - time.start
+    termy <- cumsum(tissue * dt) / tissue
+    termx <- cumsum(input.function * dt) / tissue
     
-    for (i in 1:(l-2)) {
-        lm1 <- lm(term1[(l - time.regression):l] ~ 
-                      term2[(l - time.regression):l], 
-                  weights = frame.length[(l - time.regression):l]) 
-        error[i] <- summary(lm1)$sigma        
-        time.regression <- time.regression - 1
-    }        
+    # Correct bad points (just for aesthetic purposes)
+    bad <- termx < 0 | termy < 0
+    termx[bad] <- 0
+    termy[bad] <- 0
     
-    time.regression <- l - which(error == min(error, na.rm = TRUE))    
+    # The fitting and plotting is done by this helper function
+    res <- processlinear(termx, termy, plot, main = "Logan", 
+                         xlab = "Int(Cplasma) / Ctissue [min]",
+                         ylab = "Int(Ctissue) / Ctissue [min]", ...)
     
-    # Do the regression from the chosen time point
-    lm1 <- lm(term1[(l-time.regression):l] ~ term2[(l-time.regression):l], 
-              weights = frame.length[(l-time.regression):l]) 
-        
-    # Plot, if requested
-    if (plot) {         
-        # Filled circles for taken points, empty circles for the others
-        point.types <- rep(1, l)
-        point.types[(l - time.regression):l] <- 19              
-        # Adjust margins
-        mar <- par("mar")
-        mar[2] <- mar[2] + 3        
-        par.def <- par(mar = mar)        
-        # Set the x and y axis labels to the correct equations
-        x.exp <- expression(frac(integral(C[plasma](u) * du, 0, t),
-                                 C[tissue](t))
-        )
-        y.exp <- expression(frac(integral(C[tissue](u) * du, 0, t), 
-                                 C[tissue](t))
-                            )            
-        # Plot with additional options (if provided)
-        plot(term2, term1, pch = point.types, xlab = x.exp, ylab = y.exp, ...)  
-        # Show regression line
-        abline(lm1)
-        # Return plot parameters to default values
-        par(par.def)
-    }        
-    
-    kparms = data.frame(Vt = as.numeric(lm1$coefficients[2]),
-                        intercept = as.numeric(lm1$coefficients[1]))
-    
-    # Return slope as Vt, intercept and fit object
-    list(kparms = kparms, fit = lm1)
+    return(res)
 }
