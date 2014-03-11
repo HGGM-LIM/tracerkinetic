0e28af23d9178e2f1d685eef24c7c66e6a3fcaba
José María Mateos <jmmateos@mce.hggm.es>
2014-03-11 12:38:17 +0100

    Added murase(). Needs nnls package

--- R/multilinear.R
+++ R/multilinear.R
@@ -71,9 +71,9 @@ MA2 <- function(input.function, tissue, time.start, time.end) {
     x3 <- c2
     x4 <- c1
     
-    ma2 <- lm(tissue ~ 0 + x1 + x2 + x3 + x4)
-    
+    ma2 <- lm(tissue ~ 0 + x1 + x2 + x3 + x4)    
     coefs <- as.numeric(coef(ma2))
+    
     g1 <- coefs[1]
     g2 <- coefs[2]
     g3 <- coefs[3]
@@ -120,9 +120,9 @@ MLAIR1 <- function(input.function, tissue, time.start, time.end) {
     x3 <- cumsum(tissue * dt)
     x4 <- cumsum(c1 * dt)
     
-    mlair1 <- lm(tissue ~ 0 + x1 + x2 + x3 + x4)
-    
+    mlair1 <- lm(tissue ~ 0 + x1 + x2 + x3 + x4)    
     coefs <- as.numeric(coef(mlair1))
+        
     P1 <- coefs[1]
     P2 <- coefs[2]
     P3 <- coefs[3]
@@ -161,6 +161,7 @@ MLAIR2 <- function(input.function, tissue, time.start, time.end) {
     
     mlair2 <- lm(y ~ 0 + x1 + x2 + x3 + x4)
     coefs <- as.numeric(coef(mlair2))
+        
     P1 <- coefs[1]
     P2 <- coefs[2]
     P3 <- coefs[3]
@@ -168,4 +169,35 @@ MLAIR2 <- function(input.function, tissue, time.start, time.end) {
     Ki <- P1
     
     return(list(kparms = data.frame(Ki, P1, P2, P3, P4), fit = mlair2))    
+}
+
+#' Multilinear analysis for 1-compartment model.
+#' 
+#' @param input.function Input function TAC.
+#' @param tissue Tissue TAC.
+#' @param time.start Initial acquisition time for each frame (in minutes).
+#' @param time.end Final acquisition time for each frame (in minutes).
+#' 
+#' @return A list with two fields: \code{kparms}, that contains the kinetic
+#'   parameters, and \code{fit}, which contains the linear least squares fit.
+#' 
+#' @references K. Murase, "Efficient method for calculating kinetic parameters 
+#'   using T1-weighted dynamic contrast-enhanced magnetic resonance imaging.," 
+#'   Magn. Reson. Med., vol. 51, no. 4, pp. 858-62, Apr. 2004.
+
+murase <- function(input.function, tissue, time.start, time.end) {
+    
+    dt <- time.end - time.start
+    x1 <- cumsum(input.function * dt)
+    x2 <- cumsum(tissue * dt)
+    x3 <- input.function
+        
+    lm1 <- nnls(cbind(x1, -x2, x3), tissue)
+    coefs <- lm1$x
+    
+    K1 <- coefs[1] - coefs[2]*coefs[3]
+    k2 <- coefs[2]
+    vB <- coefs[3]
+    
+    return(list(kparms = data.frame(K1, k2, vB), fit = lm1))  
 }
